13637fc84e15a5544c416a14460a0494d11b0f56
Maciek <mdyrdal@student.agh.edu.pl>
2024-01-09 23:04:12 +0100

    Add POST method handling

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 import flask
 import json
@@ -11,16 +11,28 @@ CORS(app)
 def hello():
     return "Hello, world!"
 
-@app.route("/users", methods=["GET"])
+@app.route("/users", methods=["GET", "POST"])
 def users():
-    with open(os.path.dirname(__file__) + "/../../data/users.json", "r") as f:
-        data = json.load(f)
-        data.append({
-            "username": "user4",
-            "pets": ["hamster"]
-        })
+    print("users endpoint reached...")
+    if request.method == "GET":
+        with open(os.path.dirname(__file__) + "/../../data/users.json", "r") as f:
+            data = json.load(f)
+            data.append({
+                "username": "user4",
+                "pets": ["hamster"]
+            })
 
-        return flask.jsonify(data)
+            return flask.jsonify(data)
+        
+    elif request.method == "POST":
+        received_data = request.get_json()
+        print(f"received data: {received_data}")
+        message = received_data['data']
+        return_data = {
+            "status": "success",
+            "message": f"received: {message}"
+        }
+        return flask.Response(response=json.dumps(return_data), status=201)
 
 if __name__ == "__main__":
     app.run("localhost", 6060)
